13110bca25019ab317ed5348f85a34d13c45a2c6
devingoulet <gouletd@oregonstate.edu>
2026-04-01 18:37:47 -0700

    project 1

---
+++ sample.py
@@ -0,0 +1,7 @@
+# Author: Devin Goulet
+# GitHub username: devingoulet
+# Date: 04/01/2026
+# Description: Asks the user for their name and then prints out their name
+
+your_name = input("Please enter your name: ")
+print("Your name is " + your_name + ".")
