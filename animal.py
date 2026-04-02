c3d07db6b96817e4055a944b74514274a6abf0ea
devingoulet <gouletd@oregonstate.edu>
2026-04-01 19:47:06 -0700

    project one

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Devin Goulet
+# GitHub username: devingoulet
+# Date: 04/01/2026
+# Description: Asks the user for their favorite animal and then prints their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
