81dfddb3b215a4ac19f0fe2083d67b7b82396705
devingoulet <gouletd@oregonstate.edu>
2026-04-10 18:54:07 -0700

    project 2.b

--- temp_convert.py
+++ temp_convert.py
@@ -1,3 +1,7 @@
-celsius = float(input("Please enter a Celsius temperature: "))
+print("Please enter a Celsius temperature.")
+celsius = float(input())
+
 fahrenheit = (celsius * 9/5) + 32
+
+print("The equivalent Fahrenheit temperature is:")
 print(fahrenheit)
